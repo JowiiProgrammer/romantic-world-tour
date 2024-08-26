0919aa2b47e0e48d807694ece1d213b41c0f7584
JowiiProgrammer <jowiigamer07@gmail.com>
2024-08-26 22:28:24 +0200

    First Version

---
+++ src/data/countries.js
@@ -0,0 +1,23 @@
+export const countries = [
+    'Argentina Bariloche', 'Australia', 'Canadá', 'Corea', 'Ecuador', 'Egipto', 'EE. UU.', 
+    'Inglaterra', 'Japón', 'Noruega', 'Paraguay', 'Perú', 'Roma', 'Rusia', 'Suiza', 'Venecia'
+  ];
+  
+  export const romanticMessages = {
+    'Paraguay': 'Nuestro amor es tan cálido como el abrazo guaraní y tan refrescante como el tereré.',
+    'Argentina Bariloche': 'Nuestro amor es tan apasionado como el tango y tan impresionante como los paisajes de Bariloche.',
+    'Australia': 'Nuestro amor es tan grande como el Outback y tan colorido como la Gran Barrera de Coral.',
+    'Canadá': 'Nuestro amor es tan puro como las Montañas Rocosas y tan dulce como el jarabe de arce.',
+    'Corea': 'Nuestro amor es tan armonioso como el yin y el yang en la bandera coreana.',
+    'Ecuador': 'Nuestro amor está en el centro del mundo, como la línea del Ecuador que atraviesa este hermoso país.',
+    'Egipto': 'Nuestro amor es tan antiguo como las pirámides y tan misterioso como la Esfinge.',
+    'EE. UU.': 'Nuestro amor es tan alto como el Empire State y tan libre como la Estatua de la Libertad.',
+    'Inglaterra': 'Nuestro amor es tan sólido como el Big Ben y tan vasto como los jardines ingleses.',
+    'Japón': 'Nuestro amor es tan fuerte como el Monte Fuji y tan sereno como un jardín zen.',
+    'Noruega': 'Nuestro amor brilla como las auroras boreales en el cielo noruego.',
+    'Perú': 'Nuestro amor es tan majestuoso como Machu Picchu y tan rico como la historia inca.',
+    'Roma': 'Nuestro amor es eterno, como la Ciudad Eterna.',
+    'Rusia': 'Nuestro amor es tan vasto como la tundra siberiana y tan cálido como un abrazo ruso.',
+    'Suiza': 'Nuestro amor es tan preciso como un reloj suizo y tan dulce como su chocolate.',
+    'Venecia': 'Nuestro amor fluye como los canales de Venecia, conectando nuestros corazones para siempre.'
+  };
---
+++ src/data/songs.js
@@ -0,0 +1,5 @@
+export const songs = [
+    { title: `In Case You Didn't Know`, artist: 'Brett Young', src: `/music/Brett Young - In Case You Didn't Know.mp3`, cover: 'https://i.scdn.co/image/ab67616d00001e028c67513a5ffd9a018b410f1b' },
+    { title: 'No Hay Nadie Más', artist: 'Sebastián Yatra', src: '/music/Sebastián Yatra - No Hay Nadie Más.mp3', cover: 'https://i.scdn.co/image/ab67616d00001e02a04e5df68ea4c891a4252c7e' },
+    { title: 'Momentos', artist: 'Reik', src: '/music/Reik - Momentos.mp3', cover: 'https://i.scdn.co/image/ab67616d00001e02d6cfdde09715f80ddf68ec52' },
+  ];
